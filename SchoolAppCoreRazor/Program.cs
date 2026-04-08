2507bb8c3578e3ffb4d423922f37afa5816401e3
Reason2Code <iamphupq@gmail.com>
2026-04-09 02:33:42 +0700

    step 6

--- SchoolAppCoreRazor/Program.cs
+++ SchoolAppCoreRazor/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolAppCoreRazor.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<SchoolContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDBConnectionString")));
 
 var app = builder.Build();
 
