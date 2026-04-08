69f4433108b31349cd021da5b13913b2048b0e1c
Reason2Code <iamphupq@gmail.com>
2026-04-09 02:10:55 +0700

    step 3

---
+++ SchoolAppCoreRazor/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolAppCoreRazor.Models
+{ 
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public required string LastName
+        {
+            get; set;
+        }
+        public required string FirstName
+        {
+            get; set;
+        }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment>? Enrollments
+        { get; set; }
+    }
+}
